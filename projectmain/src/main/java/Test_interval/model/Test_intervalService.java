952f4252a79e49c5782041f8d81c043b96c5f980
Shuhaotim <a25251226@gmail.com>
2016-06-15 17:19:38 +0800

    調整Edu、Class資料查詢速度... 新增於Test_interval資料,一併產生Fullcalendar明細資料

--- projectmain/src/main/java/Test_interval/model/Test_intervalService.java
+++ projectmain/src/main/java/Test_interval/model/Test_intervalService.java
@@ -227,7 +227,7 @@ public class Test_intervalService {
 			clas = classSrc.findByPrimaryKeyClass(class_id);
 			String title0 = "";
 			String title1 = "java";
-			java.util.Date test_startdate1 = null;
+			java.util.Date test_startdate1 = new Date(3600000);
 		    java.util.Date test_enddate1 = null;
 			String color0 = "#08d572";
 			String color1 = "#3A87AD";
